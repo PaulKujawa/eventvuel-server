4f9237de2e1a1534f559376680546abbc7289b43
Paul Kujawa <p.kujawa@rebuy.de>
2018-12-04 15:28:00 +0100

    watch mode for development

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,6 +3,7 @@ const Dotenv = require('dotenv-webpack');
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const NodemonPlugin = require( 'nodemon-webpack-plugin' );
 
 export const webpackConfig = () => {
     const config: Configuration = {
@@ -29,12 +30,14 @@ export const webpackConfig = () => {
             ],
         },
         plugins: [
-            new Dotenv(),
+            new Dotenv({silent: true /* dev only */}),
+            new NodemonPlugin(),
         ],
         externals: [
             nodeExternals(),
         ],
         resolve: {
+            // .mjs see https://github.com/graphql/graphql-js/issues/1272#issuecomment-377384574
             extensions: ['.ts', '.mjs', '.js'],
             plugins: [
                 new TsconfigPathsPlugin({configFile: "./tsconfig.json"}),
